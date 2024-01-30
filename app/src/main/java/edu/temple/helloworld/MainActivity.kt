eae07b92db60f2f0b8c40c19a34ae13a05651d21
Andrew Van Siclen <tuq36932@temple.edu>
2024-01-30 12:12:57 -0500

    Added event listener

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -10,17 +10,23 @@ class MainActivity : AppCompatActivity() {
 
     // Declare view properties - the first one is done for you
     lateinit var displayTextView: TextView
-
+    lateinit var displayEditText: EditText
+    lateinit var displayButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Initialize with views defined in Layout - the first one is done for you
         displayTextView = findViewById(R.id.displayTextView)
+        displayEditText = findViewById(R.id.nameEditText)
+        displayButton = findViewById(R.id.clickMeButton)
 
         
         // Respond to button click event per specifications
 
+        displayButton.setOnClickListener{
+            var name = displayEditText.text
+            displayTextView.text = "Hello, $name" }
 
     }
 }
